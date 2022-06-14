6591dda3c892c79558f43f7b3cb34a1bac74711c
Iagofernandez <43774731+Iagofernandez@users.noreply.github.com>
2022-06-14 16:16:01 +0200

    Añadiendo tablas necesarias para el programa: usuarios y notas

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ import sqlite3
 # creación base de datos
 conexion = sqlite3.connect("./20-proyecto-python/usuarios.db")
 
+# Crear tablas de usuario y notas
+tabla_usuario = conexion.cursor()
+tabla_notas = conexion.cursor()
+
+tabla_usuario.execute("""CREATE TABLE IF NOT EXISTS usuario (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre varchar(8),
+    contraseña varchar(10)
+)""")
+
+tabla_notas.execute("""CREATE TABLE IF NOT EXISTS notas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo varchar(10),
+    contenido varchar(120),
+    nombre varchar(8)
+)""")
 
 accion_usuario = int(input("Que acción quieres realizar \n1.Logueo \n2.Registro \n3.Salir \n"))
 while accion_usuario >=1:
@@ -31,3 +47,5 @@ while accion_usuario >=1:
     else :
         print("Opcion no valida")
         accion_usuario = int(input("introduce una opción válida \n"))
+
+conexion.close()
